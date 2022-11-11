1c01338e4ac4cc5eb88c3f57d060be072c24ae53
muhammadkhanusmanov <uqaxromon@gmail.com>
2022-11-11 11:14:18 +0500

    01

--- get_cheapest_fruit.py
+++ get_cheapest_fruit.py
@@ -7,5 +7,19 @@ def get_cheapest_fruit(data:str)->str:
     returns:
         name of the cheapest fruit
     """
+    row=data.split('\n')[1:]
+    values=[]
+    ids=[]
+    for i in range(len(row)):
+        values.append(float(row[i].split(',')[1]))
+        ids.append(row[i].split(',')[0])
+    m=values[0]
+    for j in values:
+        if m>j:
+            m=j
+    idindex=values.index(m)
+
+    return ids[idindex]
     # your code here
-    pass
+f = open('fruits.csv').read()
+print(get_cheapest_fruit(f))
